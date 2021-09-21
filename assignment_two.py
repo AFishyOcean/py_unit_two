bbd5fc753b98eb007c47423f8a027fe66fc000a9
AFishyOcean <dljhazelh@gmail.com>
2021-09-21 14:31:01 -0400

    Worked on assignment 2

---
+++ assignment_two.py
@@ -0,0 +1,15 @@
+name = input('Hi whats your name?')
+print('Well hello', name)
+
+fav_number = float(input('What is your favorite number?'))
+as_big = (fav_number / 42)
+print('Oh cool that is', as_big, 'times as big as my favorite number which is 42')
+
+car = input('What would you say your dream car is?')
+print('Sounds like a cool car')
+car_price = float(input('How much do you think it would cost?'))
+print('Well thats not exactly cheap is it')
+morgage_time = int(input('How many years do you think it would take you to pay off a loan for it?'))
+percent_int = float(input('What do you think the annual interest would be? (In decimals please)'))
+monthly_int = (percent_int / 12)
+total_cost = ((car_price * monthly_int) / (1 - ( 1 + monthly_int) ** -morgage_time))
